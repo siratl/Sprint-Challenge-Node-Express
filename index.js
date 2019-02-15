7389372a22d65db1d20fac37e4bc61916ea6e246
siratl <elishubax@gmail.com>
2019-02-15 09:45:10 -0800

    Configured npm script server and start executing code using nodemon and node respectively. Implimented GET in CRUD operations

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const server = require('./api/server')
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(`\n*** Server running on Port: ${port} ***\n`)
+})
