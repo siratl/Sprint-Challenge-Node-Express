a22c31b5a2868a87001c98e8beb3339310b3886e
siratl <elishubax@gmail.com>
2019-02-15 11:02:00 -0800

    Completed MVP

--- data/actions/actionsRouter.js
+++ data/actions/actionsRouter.js
@@ -1,33 +1,109 @@
 const express = require('express');
 
-const Actions = require('../helpers/actionModel')
+const Actions = require('../helpers/actionModel');
 
-const router = express.Router()
+const Projects = require('../helpers/projectModel');
+
+const router = express.Router();
 
 //************************** GET ALL ACTIONS ************************/
 router.get('/', async (req, res) => {
-    Actions.get()
-      .then(action => {
-        res.status(200).json({ action });
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'Cannot retrieve Actions.' });
-      });
-  });
-
+  Actions.get()
+    .then(action => {
+      res.status(200).json({ action });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Cannot retrieve Actions.' });
+    });
+});
 
 //************************** GET SPECIFIC ACTION *************************/
-
-
+router.get('/:id', (req, res) => {
+  Actions.get(req.params.id)
+    .then(action => {
+      res.status(200).json({ action });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The action could not be retrieved.' });
+    });
+});
 
 //************************** ADD NEW ACTION *************************/
+router.post('/', (req, res) => {
+  const { project_id, description, notes, completed } = req.body;
+  const newAction = { project_id, description, notes, completed };
 
+  if (newAction.description.length > 128) {
+    return res.status(412).json({
+      errorMessage: 'Action description is more than 128 characters.',
+    });
+  }
+  if (!description || !notes) {
+    return res.status(417).json({
+      errorMessage:
+        'Description, project ID, and notes required for your action.',
+    });
+  } else {
+    Projects.get(req.params.id)
+      .then(project => {
+        if (project) {
+          Actions.insert(newAction)
+            .then(newAction => {
+              res
+                .status(201)
+                .json({ newAction, message: 'Action created successfully' });
+            })
+            .catch(err => {
+              res.status(500).json({ error: 'The Action could not be saved.' });
+            });
+        } else {
+          res.status(500).json({ error: 'No Valid project ID found.' });
+        }
+      })
 
+      .catch(() =>
+        res.status(500).json({ error: 'The Action could not be saved.' }),
+      );
+  }
+});
 
 //************************** UPDATE ACTION *************************/
-
-
+router.put("/:id", (req, res) => {
+    const { notes, description } = req.body;
+    if (!description || !notes) {
+      res.status(422).json({ message: "Description and Notes are required" });
+    } else {
+      Actions.update(req.param.id, { notes, description })
+        .then(res => {
+          if (res) {
+            Actions.get().then(action => {
+              res.json(action);
+            });
+          } else {
+            res.status(404).json({ message: "Actions Update Failed." });
+          }
+        })
+        .catch(() => {
+          res.status(500).json({ message: "The Action could not be updated" });
+        });
+    }
+  });
 
 //************************** DELETE ACTION *************************/
+router.delete("/:id", (req, res) => {
+    Actions.remove(req.params.id)
+      .then(res => {
+        if (res) {
+          Actions.get().then(action => {
+            res.json(action);
+          });
+        } else {
+          res.status(400).json({ message: `Failed to delete action with id ${req.params.id}` });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ message: "The Action could not be deleted" });
+      });
+  });
 
 module.exports = router;
--- data/projects/projectsRouter.js
+++ data/projects/projectsRouter.js
@@ -1,33 +1,122 @@
 const express = require('express');
 
-const Projects = require('../helpers/projectModel')
+const Projects = require('../helpers/projectModel');
 
-const router = express.Router()
+const router = express.Router();
 
 //************************** GET ALL PROJECTS ************************/
 router.get('/', async (req, res) => {
-    Projects.get()
-      .then(project => {
-        res.status(200).json({ project });
-      })
-      .catch(err => {
-        res.status(500).json({ error: 'Cannot retrieve Project.' });
-      });
-  });
-
+  Projects.get()
+    .then(project => {
+      res.status(200).json({ project });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Cannot retrieve Project.' });
+    });
+});
 
 //************************** GET SPECIFIC PROJECT *************************/
+router.get('/:id', (req, res) => {
+  Projects.get(req.params.id)
+    .then(project => {
+      res.status(200).json({ project });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The project could not be retrieved.' });
+    });
+});
 
+//************************** GET PROJECT ACTIONS *************************/
+router.get('/actions/:id', (req, res) => {
+  const { id } = req.params;
 
+  Projects.get(id)
+    .then(project => {
+      if (project) {
+        Projects.getProjectActions(id).then(actions => {
+          res.json(actions);
+        });
+      } else {
+        res.status(404).json({
+          errorMessage: `The project with the specified ID '${id}' does not exist.`,
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: `The Specified project '${id}' action(s) could not be retrieved.`,
+      });
+    });
+});
 
 //************************** ADD NEW PROJECT *************************/
+router.post('/', (req, res) => {
+  const { name, description } = req.body;
 
-
+  if (!name || !description) {
+    res
+      .status(422)
+      .json({ errorMessage: 'Name and Description Required to add a project' });
+  } else {
+    Projects.insert({ name, description })
+      .then(res => {
+        if (res) {
+          Projects.get().then(project => {
+            res.json(project);
+          });
+        }
+      })
+      .catch(() => res.status(500).json({ error: 'Server Error' }));
+  }
+});
 
 //************************** UPDATE PROJECT *************************/
+router.put('/:id', (req, res) => {
+  const { name, description } = req.body;
 
-
+  if (!name || !description) {
+    res.status(422).json({ errorMessage: 'Name and Description Required' });
+  } else {
+    Projects.get(req.params.id)
+      .then(project => {
+        if (project) {
+          Projects.update(req.params.id, { name, description }).then(res => {
+            if (res) {
+              Projects.get().then(project => {
+                es.json({ message: 'User updated', project });
+              });
+            } else {
+              res
+                .status(404)
+                .json({
+                  errorMessage:
+                    'The project with the specified ID does not exist.',
+                });
+            }
+          });
+        } else {
+          res.status(404).json({ errorMessage: 'Project Not Found' });
+        }
+      })
+      .catch(() =>
+        res
+          .status(500)
+          .json({ error: 'The user information could not be modified.' }),
+      );
+  }
+});
 
 //************************** DELETE PROJECT *************************/
+router.delete('/:id', (req, res) => {
+  Projects.remove(req.params.id)
+    .then(res => {
+      if (res) {
+        Projects.get().then(deleted => res.json(deleted));
+      } else {
+        res.status(500).json({ errorMessage: 'Failed to delete project' });
+      }
+    })
+    .catch(() => res.status(500).json({ error: 'Server Error' }));
+});
 
 module.exports = router;
